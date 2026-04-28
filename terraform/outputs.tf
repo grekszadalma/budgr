a0308e6a3863e11358dedb414f30c82bb2b3c084
grekszadalma <grekszadalma@gmail.com>
2026-04-28 18:11:32 +0200

    Change Terraform outputs and S3 terraform

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,6 +2,10 @@ output "ec2_ip" {
   value = aws_instance.app.public_ip
 }
 
+output "cloudfront_id" {
+  value = aws_cloudfront_distribution.cdn.id
+}
+
 output "cloudfront_url" {
   value = aws_cloudfront_distribution.cdn.domain_name
 }
--- terraform/s3.tf
+++ terraform/s3.tf
@@ -5,6 +5,22 @@ resource "aws_s3_bucket" "frontend" {
 resource "aws_s3_bucket_public_access_block" "frontend" {
   bucket = aws_s3_bucket.frontend.id
 
-  block_public_acls   = false
-  block_public_policy = false
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "frontend_public_read" {
+  bucket = aws_s3_bucket.frontend.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect    = "Allow"
+      Principal = "*"
+      Action    = "s3:GetObject"
+      Resource  = "${aws_s3_bucket.frontend.arn}/*"
+    }]
+  })
 }
