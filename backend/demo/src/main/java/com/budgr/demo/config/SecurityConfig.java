96f2fdc22a1bf281a14ab2a5c05dbe78347fa6e6
grekszadalma <grekszadalma@gmail.com>
2026-05-01 19:38:20 +0200

    Change backend URL in frontend

--- backend/demo/src/main/java/com/budgr/demo/config/SecurityConfig.java
+++ backend/demo/src/main/java/com/budgr/demo/config/SecurityConfig.java
@@ -71,7 +71,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of(
+                "http://localhost:5173",
+                "https://d3ff8ih0471bsz.cloudfront.net"
+        ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,1 +1,1 @@
-export const API_URL = import.meta.env.VITE_API_URL;
+export const API_URL = "http://13.38.91.236:8080";
